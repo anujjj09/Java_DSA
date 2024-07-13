0b40e97af840803847b95f6b013bd5cf6ce02d20
Anuj Sharma <anuj@Anujs-MacBook-Air.local>
2024-07-13 12:35:12 +0530

    first commit

---
+++ Day1/Flowcharts.java
@@ -0,0 +1,14 @@
+package Day1;
+import java.util.Scanner;
+class Flowcharts {
+    public static void main(String[] args) {
+        //input
+        Scanner sc = new Scanner (System.in);
+        int a= sc.nextInt();
+
+        //output
+        System.out.println("value input by user is");
+        System.out.print(a);
+        sc.close();
+    }
+}
