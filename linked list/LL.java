ebe4ed05e14d3e0166391bbd5008368333649a59
Anuj Sharma <anujjsharma09@gmail.com>
2026-09-26 05:32:57 +0530

    LL leetcode

--- linked list/LL.java
+++ linked list/LL.java
@@ -265,7 +265,23 @@ public class LL {
  * }
  */
 // public class Solution {
-//     public boolean hasCycle(ListNode head) {
+    // public boolean hasCycle(ListNode head) {
+        // ListNode fast = head;
+        // ListNode slow = head;
+
+        // while(fast != null && fast.next != null){
+        //     fast = fast.next.next;
+        //     slow = slow.next;
+        //     if(slow == fast){
+        //         return true;
+        //     }
+        // }
+        // return false;
+    // }
+// }
+
+//find length of a cycle
+//     public int LengthCycle(ListNode head){
 //         ListNode fast = head;
 //         ListNode slow = head;
 
@@ -273,12 +289,116 @@ public class LL {
 //             fast = fast.next.next;
 //             slow = slow.next;
 //             if(slow == fast){
-//                 return true;
+//                 // calculate length
+//                 ListNode temp = slow;
+//                 int length = 0;
+//                 do{
+//                     temp = temp.next;
+//                     length++;
+//                 } while(temp != slow);
+//                 return length;
 //             }
 //         }
-//         return false;
+//         return 0;
 //     }
-// }
+
+
+// //q142 length of cycle
+
+//     public ListNode detectCycle(ListNode head) {
+//         int length = 0;
+//         ListNode fast = head;
+//         ListNode slow = head;
+
+//         while(fast != null && fast.next != null){
+//             fast = fast.next.next;
+//             slow = slow.next;
+//             if(slow == fast){
+//                 length = LengthCycle(slow);
+//                 break;
+//             }
+
+//         }
+        
+        
+//         //find start node
+//         ListNode f = head;
+//         ListNode s = head;
+//         if (length==0){
+//             return null;
+
+//         }
+
+//         while(length>0){
+//             s = s.next;
+//             length--;
+
+//         }
+
+//         // keep moving both forward one by one till meet
+
+//         while(f != s){
+//             f = f.next;
+//             s=s.next;
+
+//         }
+//         return s;
+        
+//     }
+
+
+
+//q 202 HAPPY NUMBER
+    // public boolean isHappy(int n) {
+    //     int slow = n;
+    //     int fast = n;
+
+        
+    //     do{
+    //         slow = findSquare(slow);
+    //         fast = findSquare(findSquare(fast));
+
+    //     }while(slow!= fast);
+
+    //     if(slow == 1){
+    //         return true;
+    //     } return false;
+
+
+
+    // }
+
+    // private int findSquare(int number){
+    //     int ans = 0;
+    //     while(number > 0){
+    //         int rem = number % 10;
+    //         ans = rem*rem;
+    //         number /= 10;
+
+    //     }
+    //     return ans;
+    // }
+
+//q876 middle of LL
+    // public ListNode middleNode(ListNode head) {
+    //     ListNode slow = head;
+    //     ListNode fast = head;
+
+    //     while(fast != null &&  fast.next != null){
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+        
+    //     }
+    //     return slow;
+    // }
+
+
+// q148 sort LL using merge/bubble sort
+    
+
+
+
+
     private class Node{
         private int value;
         private Node next;
